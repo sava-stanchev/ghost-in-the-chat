81e400343d69670365560ecbd861547944dd5b5c
Sava Stanchev <s.stanchev94@gmail.com>
2023-03-21 10:28:16 +0200

    added delete chat function

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/firebase";
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { collection } from "firebase/firestore";
+import { collection, deleteDoc, doc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -26,13 +26,18 @@ function ChatRow({ id }: Props) {
     setActive(pathname.includes(id));
   }, [pathname]);
 
+  const removeChat = async () => {
+    await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
+    router.replace("/");
+  };
+
   return (
     <Link href={`/chat/${id}`} className={`chat-row ${active && "active"}`}>
       <ChatBubbleLeftIcon className="chat-bubble" />
       <p className="chat-row-name">
         {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
       </p>
-      <TrashIcon className="trash-icon" />
+      <TrashIcon onClick={removeChat} className="trash-icon" />
     </Link>
   );
 }
