609f401332da9af792e43986eee7927fefac357b
Sava Stanchev <s.stanchev94@gmail.com>
2023-03-21 19:14:00 +0200

    added response message to chat questions

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import query from "@/lib/queryApi";
 import type { NextApiRequest, NextApiResponse } from "next";
+import admin from "firebase-admin";
+import { adminDb } from "@/firebaseAdmin";
 
 type Data = {
   answer: string;
@@ -23,5 +25,25 @@ export default async function handler(
   }
 
   const response = await query(prompt, model);
-  res.status(200).json({ answer: "John Doe" });
+
+  const message: Message = {
+    text: response || "The Ghost was unable to find an answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "Ghost",
+      name: "Ghost",
+      avatar:
+        "https://images.unsplash.com/photo-1606522109608-e5b0c3166605?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3OTQxODM5Nw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+    },
+  };
+
+  await adminDb
+    .collection("users")
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
 }
