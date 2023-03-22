f5af1517f3029b63ac19e132ea89221f0e2a0034
Sava Stanchev <s.stanchev94@gmail.com>
2023-03-22 16:32:53 +0200

    remove unneeded files and fix message avatar css

--- components/Message.tsx
+++ components/Message.tsx
@@ -10,7 +10,7 @@ function Message({ message }: Props) {
   return (
     <div className={`py-5 text-white ${isGhost && "bg-[#434654]"}`}>
       <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-        <img src={message.user.avatar} alt="" className="h-8 w-8" />
+        <img src={message.user.avatar} alt="" className="h-8 w-8 min-w-[2rem]" />
         <p className="pt-1 text-sm">{message.text}</p>
       </div>
     </div>
