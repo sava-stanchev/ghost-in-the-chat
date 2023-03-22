ab9a860babd06151ed27653ab08e0990e5efdb38
Sava Stanchev <s.stanchev94@gmail.com>
2023-03-22 09:29:02 +0200

    added empty chat instruction

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -5,6 +5,7 @@ import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   chatId: string;
@@ -16,13 +17,28 @@ function Chat({ chatId }: Props) {
   const [messages] = useCollection(
     session &&
       query(
-        collection(db, "users", session?.user?.email!, "chats", chatId, "messages"),
+        collection(
+          db,
+          "users",
+          session?.user?.email!,
+          "chats",
+          chatId,
+          "messages"
+        ),
         orderBy("createdAt", "asc")
       )
   );
 
   return (
     <div className="chat-container">
+      {messages?.empty && (
+        <>
+          <p className="chat-instruction">
+            Type a message below to get started!
+          </p>
+          <ArrowDownCircleIcon className="chat-instruction-icon" />
+        </>
+      )}
       {messages?.docs.map((message) => (
         <Message key={message.id} message={message.data()} />
       ))}
